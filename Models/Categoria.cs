a567b33035150b202816471e761ac90ffb000c5e
= <=>
2022-11-25 15:40:41 -0500

    Agregar archivos de proyecto.

---
+++ Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿namespace proyectoef.Models
+{
+    public class Categoria
+    {
+        public Guid CategoriaId { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        //Propiedad para relacionarla con Tarea, de tipo icolelction
+        //Con esta propiedad se puede traer todas las tareas asociadas a esa categoría
+        public virtual ICollection<Tarea> Tareas { get; set; }
+
+    }
+}
---
+++ Models/Tarea.cs
@@ -0,0 +1,28 @@
+﻿namespace proyectoef.Models
+{
+    //Cada Tarea va a tener asignada una categoria
+    public class Tarea
+    {
+        public Guid TareaId { get; set; }
+        //Relación de la tarea con la categoria
+        public Guid CategoriaId { set; get; }
+        public string Titulo { set; get; }
+        public string Descripcion { set; get; }
+        public Prioridad PrioridadTarea { set; get; }   
+        public DateTime FechaCreacion { set; get; } 
+
+        //Propiedad para relacionarla con Categoria
+        public virtual Categoria Categoria { set; get; }
+
+
+    }
+
+    public enum Prioridad
+    {
+        Baja,
+        Media,
+        Alta
+    }
+
+
+}
